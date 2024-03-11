070284c40e0b161bdbb653a2910f0cc0b9c0197d
Yash Hegde <yash4success@gmail.com>
2024-03-11 23:25:28 +0530

    pics scroll added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import { ScrollPics} from './components/Scrollbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <div>
+        <ScrollPics/>
+      </div>
      
     </div>
   )
