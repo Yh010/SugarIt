621e3ae417daa31a5d355b4ec794dd831a7a20a0
Yash Hegde <yash4success@gmail.com>
2024-03-14 00:56:25 +0530

    routing and basic page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Menu from './pages/Menu/Menu.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/menu",
+    element: <Menu/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
