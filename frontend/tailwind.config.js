f9d27fe82287007ca100bf5ff4d069b4e17967f1
Yash Hegde <yash4success@gmail.com>
2024-03-14 05:48:22 +0530

    About Us added and tailwind config fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,10 @@
 import withMT from "@material-tailwind/react/utils/withMT";
 
 export default withMT({
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
