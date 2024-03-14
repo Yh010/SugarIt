cc385ca935a6b46886ae6041968f7beaacba85ca
Yash Hegde <yash4success@gmail.com>
2024-03-14 23:42:09 +0530

    Modal creation attempts

---
+++ frontend/src/components/TodaySpecial.jsx
@@ -0,0 +1,15 @@
+
+ export default function TodaysSpecial() {
+    return (
+        <div>
+            <button onClick={() => {
+                console.log("hi")
+            }}>
+                click here
+            </button>
+                 
+            
+        </div>
+    )
+} 
+
