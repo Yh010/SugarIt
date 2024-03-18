dacda01b0b96390033f4ce3a9fd5ded3a15eb66d
Yash Hegde <yash4success@gmail.com>
2024-03-18 13:29:19 +0530

    Add to cart button

--- frontend/src/pages/Menu/MenuBox.jsx
+++ frontend/src/pages/Menu/MenuBox.jsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/prop-types */
+
+import AddtoCartButton from "../../components/PaymentComponents/AddtoCartButton"
+
 // eslint-disable-next-line react/prop-types
 const MenuBox = ({items}) => {
   return (
@@ -9,11 +12,18 @@ const MenuBox = ({items}) => {
           <article key={id} className="menu-item grid grid-cols-2 gap-y-4 gap-x-4 max-w-md">
             <img src={img} alt={title} className="photo object-fit-cover w-full h-52 border-black border rounded-md block" />
             <div className="item-info">
-              <header className="flex justify-between border-b-[1px] border-dotted border-black">
-                <h4 className="font-bold">{title}</h4>
-                <h4 className="price text-black font-bold">N{price}</h4>
-              </header>
-              <p className="item-text text-sm">{desc}</p>
+              <div className="justify-between">
+                  <div>
+                    <header className="flex justify-between border-b-[1px] border-dotted border-black">
+                      <h4 className="font-bold">{title}</h4>
+                      <h4 className="price text-black font-bold">Rs {price}</h4>
+                    </header>
+                    <p className="item-text text-sm">{desc}</p>
+                  </div>
+                  <div >
+                    <AddtoCartButton/>
+                  </div>
+              </div>
             </div>
           </article>
         )
