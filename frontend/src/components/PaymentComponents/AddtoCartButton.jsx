162aafea5224f432d901d8d5e43d3eccb167ab37
Yash Hegde <yash4success@gmail.com>
2024-03-18 08:35:08 +0530

    Payments init

---
+++ frontend/src/components/PaymentComponents/AddtoCartButton.jsx
@@ -0,0 +1,7 @@
+export default function AddtoCartButton() {
+    return (
+        <div>
+            AddtoCartButton
+        </div>
+    )
+}
---
+++ frontend/src/components/PaymentComponents/ViewReviewsButton.jsx
@@ -0,0 +1,7 @@
+export default function ViewReviewsButton() {
+    return (
+        <div>
+            ViewReviewsButton
+        </div>
+    )
+}
