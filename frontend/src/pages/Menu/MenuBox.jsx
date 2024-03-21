a189daf02789a427d211dffd4814d02619893ebd
Yash Hegde <yash4success@gmail.com>
2024-03-21 18:14:21 +0530

    veg non veg icon added

--- frontend/src/pages/Menu/MenuBox.jsx
+++ frontend/src/pages/Menu/MenuBox.jsx
@@ -8,7 +8,7 @@ const MenuBox = ({items}) => {
   return (
     <div className=" section-center w-full mx-auto max-w-screen-xl grid grid-cols-2 gap-y-8 gap-x-8 justify-items-center">
       {items.map((menuItem) => {
-        const { id, title, img, price, desc } = menuItem
+        const { id, title, img, price, desc,foodtype } = menuItem
         return (
           <article key={id} className="menu-item w-fit grid grid-cols-2 gap-y-6 gap-x-4 ">
             <img src={img} alt={title} className="photo object-cover w-full h-52 border-solid border-gold border-4" />
@@ -19,7 +19,24 @@ const MenuBox = ({items}) => {
                       <h4 className="font-bold">{title}</h4>
                       <h4 className="price text-black font-bold text-gold">Rs {price}</h4>
                     </header>
-                    <p className="item-text text-sm text-grey">{desc}</p>
+                  <p className="item-text text-sm text-grey">{desc}</p>
+                  <p>
+                    {foodtype === "non veg" ? (
+                        <img
+                          width="48"
+                          height="48"
+                          src="https://img.icons8.com/color/48/non-vegetarian-food-symbol.png"
+                          alt="non-vegetarian-food-symbol"
+                        />
+                      ) : (
+                        <img
+                          width="48"
+                          height="48"
+                          src="https://img.icons8.com/color/48/vegetarian-food-symbol.png"
+                          alt="vegetarian-food-symbol"
+                        />
+                      )}
+                  </p>
                   </div>
               </div>
               <div  className="flex justify-start">
